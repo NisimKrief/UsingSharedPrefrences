a6ea21886c113bd591280b7fa1651bfe966b005a
NisimKrief <nk0787@bs.amalnet.k12.il>
2023-04-02 19:38:20 +0300

    Fixed Tafrit klali not showing in "Credits"

--- app/src/main/java/com/example/usingsharedprefrences/MainActivity2.java
+++ app/src/main/java/com/example/usingsharedprefrences/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.usingsharedprefrences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 /**
@@ -14,6 +15,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     /**
